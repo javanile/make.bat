6147e5354c6959e1a4473a6f1f4e2c576d0e92e1
Francesco Bianco <info.francescobianco@gmail.com>
2020-09-01 09:10:43 +0200

    Update make.bat

--- make.bat
+++ make.bat
@@ -1,5 +1,17 @@
 @echo off
 
+docker run --rm ^
+    -v "%CD%:/pwd" ^
+    -v //var/run/docker.sock:/var/run/docker.sock ^
+    -v //usr/bin/docker:/usr/bin/docker ^
+    javanile/pwd
+
+type pwd.var
+
+docker-compose -f pwd.yml run --rm pwd
+
+type pwd.var
+
 docker run -it --rm ^
     -v %CD%:/make ^
     -v //var/run/docker.sock:/var/run/docker.sock ^
