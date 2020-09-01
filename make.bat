ece1ef632e1bde8ff9eec8affd1a090da5b1e2a4
Francesco Bianco <info.francescobianco@gmail.com>
2020-09-01 09:49:07 +0200

    Update make.bat

--- make.bat
+++ make.bat
@@ -1,13 +1,13 @@
 @echo off
 
-@docker run --rm ^
+docker run --rm ^
     -v "%CD%:/pwd" ^
     -v //var/run/docker.sock:/var/run/docker.sock ^
     -v //usr/bin/docker:/usr/bin/docker ^
     javanile/pwd
 
-@set /p PWD=<pwd.var
-@del pwd.var 
+set /p PWD=<pwd.var
+del pwd.var 
 
 docker run -it --rm ^
     -w "%PWD%" ^
