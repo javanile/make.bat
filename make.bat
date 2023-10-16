46a8159d5117e999ce2347721ff6aac9e81ea745
francesco <info.francescobianco@gmail.com>
2023-10-16 15:35:50 +0200

    Fix docker version make.bat

--- make.bat
+++ make.bat
@@ -7,10 +7,12 @@ docker run --rm ^
     javanile/pwd > .pwd.var
 
 set /p PWD=<.pwd.var
+del .pwd.var
 
 docker run -ti --rm ^
     -w "%PWD%" ^
     -v "%CD%:%PWD%" ^
     -v //var/run/docker.sock:/var/run/docker.sock ^
     -v //usr/bin/docker:/usr/bin/docker ^
+    -e "WINDOWS_VOLUME=%CD%:%PWD%"
     javanile/make.bat:20 %*
