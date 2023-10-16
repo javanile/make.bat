fc215a4cd95b976276632769f0774b70552d9a2c
Francesco Bianco <info.francescobianco@gmail.com>
2023-10-16 16:22:02 +0200

    Fix file encoding

--- make.bat
+++ make.bat
@@ -1,18 +1,13 @@
 @echo off
 
-docker run --rm ^
-    -v "%CD%:/pwd" ^
-    -v //var/run/docker.sock:/var/run/docker.sock ^
-    -v //usr/bin/docker:/usr/bin/docker ^
-    javanile/pwd > .pwd.var
+set VOL1=//var/run/docker.sock:/var/run/docker.sock
+set VOL2=//usr/bin/docker:/usr/bin/docker
+
+call docker run --rm -v "%CD%:/pwd" -v "%VOL1%" -v "%VOL2%" javanile/pwd > .pwd.var
 
 set /p PWD=<.pwd.var
 del .pwd.var
 
-docker run -ti --rm ^
-    -w "%PWD%" ^
-    -v "%CD%:%PWD%" ^
-    -v //var/run/docker.sock:/var/run/docker.sock ^
-    -v //usr/bin/docker:/usr/bin/docker ^
-    -e "WINDOWS_VOLUME=%CD%:%PWD%"
-    javanile/make.bat:20 %*
+call docker run -ti --rm -w "%PWD%" ^
+    -v "%CD%:%PWD%" -v "%VOL1%" -v "%VOL2%" ^
+    -e "WINDOWS_VOLUME=%CD%:%PWD%" javanile/make.bat:20 %*
